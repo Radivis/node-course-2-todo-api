c4afc1b2e9bee5492d852d1e9e5d31ce4f51ff66
Radivis <radivis@radivis.com>
2018-01-02 21:49:03 +0100

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 // Mongoose config
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoAoo');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoAoo');
 
 module.exports = {mongoose};
